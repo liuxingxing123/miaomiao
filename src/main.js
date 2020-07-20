4de3a1020a9165ae3fb67aa1abca62e92a336873
liuxingxing123 <lxxlol@Foxmail.com>
2020-07-20 10:52:20 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,6 +10,24 @@ Vue.filter("setWH", (url, arg) => {
     return url.replace(/w\.h/, arg);
 })
 
+/**
+ * 人性化时间处理 传入时间戳
+ */
+function beautify_time(timestamp){
+    var mistiming = Math.round(new Date() / 1000)-timestamp;
+    var postfix = mistiming>0 ? '前' : '后'
+    mistiming = Math.abs(mistiming)
+    var arrr = ['年','个月','星期','天','小时','分钟','秒'];
+    var arrn = [31536000,2592000,604800,86400,3600,60,1];
+ 
+    for(var i=0; i<7; i++){
+        var inm = Math.floor(mistiming/arrn[i])
+        if(inm!=0){
+            return inm+arrr[i] + postfix
+        }
+    }
+}
+
 import Scroller from "@/components/Scroller";
 Vue.component("Scroller", Scroller)
 
